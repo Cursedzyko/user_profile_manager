34f99855230dff3bc83d8cbb237ed05192f399b0
Zafarbek Yunusov <zyunusov30@gmail.com>
2025-01-30 13:43:27 +0500

    login route created and works

--- backend/user_prof_manager_app/views.py
+++ backend/user_prof_manager_app/views.py
@@ -2,6 +2,9 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import update_last_login
 from .serializers import UserSerializer
 
 class SignupAPIView(APIView):
@@ -15,3 +18,27 @@ class SignupAPIView(APIView):
             return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
         print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginAPIVie(APIView):
+    permission_classes = [AllowAny]
+    
+    def post(self, request,  *args, **kwargs):
+        email = request.data.get("email")
+        password = request.data.get("password")
+        
+        user = authenticate(request, email=email, password=password)
+        
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            update_last_login(None, user)
+            
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+                'user': UserSerializer(user).data
+            })
+        else:
+            return Response(
+                {"error" : "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED
+            )
+            
